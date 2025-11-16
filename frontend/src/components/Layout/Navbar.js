77cff4bfed1e39d47d03933ef333137bb07b8c82
bvggies <91506641+bvggies@users.noreply.github.com>
2025-11-16 08:52:15 +0400

    Remove Xstream text after logo in navbar

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -20,9 +20,8 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <Link to="/" className="flex items-center space-x-2">
+          <Link to="/" className="flex items-center">
             <img src="/logo.png" alt="Xstream" className="h-10 w-auto" />
-            <span className="text-xl font-bold text-white">Xstream</span>
           </Link>
 
           {/* Desktop Navigation */}
